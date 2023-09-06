ec943e95f72bbdf3cc7b91fdc97109e3c12fd698
Natchanon Manatphaiboon <natchanon.space@gmail.com>
2023-09-06 14:10:22 +0700

    basic template

--- app/src/main/java/com/example/bodycomposition/fragment/GuestInputFragment.kt
+++ app/src/main/java/com/example/bodycomposition/fragment/GuestInputFragment.kt
@@ -6,8 +6,12 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.example.bodycomposition.R
+import com.example.bodycomposition.databinding.FragmentGuestInputBinding
 
 class GuestInputFragment : Fragment() {
+
+    private var binding: FragmentGuestInputBinding? = null
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
---
+++ app/src/main/java/com/example/bodycomposition/fragment/VisualizeFragment.kt
@@ -0,0 +1,18 @@
+package com.example.bodycomposition.fragment
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+class VisualizeFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return super.onCreateView(inflater, container, savedInstanceState)
+    }
+}
