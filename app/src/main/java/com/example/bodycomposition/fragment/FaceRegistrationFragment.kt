d45fd80ea502062b13a84444637e096ef393efb9
Natchanon Manatphaiboon <natchanon.space@gmail.com>
2023-09-13 11:59:17 +0700

    prepare for ml kit face detection for face extraction

--- app/src/main/java/com/example/bodycomposition/fragment/FaceRegistrationFragment.kt
+++ app/src/main/java/com/example/bodycomposition/fragment/FaceRegistrationFragment.kt
@@ -1,15 +1,27 @@
 package com.example.bodycomposition.fragment
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.camera.core.CameraSelector
+import androidx.camera.core.ImageCapture
+import androidx.camera.core.Preview
+import androidx.camera.lifecycle.ProcessCameraProvider
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import com.example.bodycomposition.databinding.FragmentFaceRegistrationBinding
+import com.example.bodycomposition.utils.RequirePermissions
+import java.util.concurrent.ExecutorService
 
 class FaceRegistrationFragment : Fragment() {
 
     private lateinit var binding: FragmentFaceRegistrationBinding
+
+    private var imageCapture: ImageCapture? = null
+
+    private lateinit var cameraExecutor: ExecutorService
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -28,5 +40,47 @@ class FaceRegistrationFragment : Fragment() {
         binding.apply {
 
         }
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        if (RequirePermissions.allPermissionsGranted(requireContext())) {
+            startCamera()
+        }
+    }
+
+    private fun startCamera() {
+        val cameraProviderFuture = ProcessCameraProvider.getInstance(requireContext())
+
+        cameraProviderFuture.addListener({
+            // Used to bind the lifecycle of cameras to the lifecycle owner
+            val cameraProvider: ProcessCameraProvider = cameraProviderFuture.get()
+
+            // Preview binding
+            val preview = Preview.Builder()
+                .build()
+                .also {
+                    it.setSurfaceProvider(binding.viewFinder.surfaceProvider)
+                }
+
+            imageCapture = ImageCapture.Builder().build()
+
+            val cameraSelector = CameraSelector.DEFAULT_FRONT_CAMERA
+
+            try {
+                // Unbind use cases before rebinding
+                cameraProvider.bindToLifecycle(
+                    this, cameraSelector, preview, imageCapture
+                )
+            } catch (exc: Exception) {
+                Log.e("Error", "Use case binding failed", exc)
+            }
+        }, ContextCompat.getMainExecutor(requireContext()))
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        cameraExecutor.shutdown()
     }
 }
