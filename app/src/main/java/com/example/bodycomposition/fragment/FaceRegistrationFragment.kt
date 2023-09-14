0e47f4b03a000a2c136ed36fa2a2bb169e1c874f
Natchanon Manatphaiboon <natchanon.space@gmail.com>
2023-09-14 16:00:11 +0700

    add takePicture and TODOs in face registration

--- app/src/main/java/com/example/bodycomposition/fragment/FaceRegistrationFragment.kt
+++ app/src/main/java/com/example/bodycomposition/fragment/FaceRegistrationFragment.kt
@@ -1,12 +1,23 @@
 package com.example.bodycomposition.fragment
 
+import android.content.ContentValues
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
+import android.graphics.ImageFormat
 import android.graphics.Rect
 import android.graphics.RectF
+import android.graphics.YuvImage
+import android.media.Image
+import android.net.Uri
+import android.os.Build
 import android.os.Bundle
+import android.os.Environment
+import android.provider.MediaStore
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.camera.core.CameraSelector
 import androidx.camera.core.ExperimentalGetImage
 import androidx.camera.core.ImageAnalysis
@@ -21,7 +32,12 @@ import com.example.bodycomposition.utils.RequirePermissions
 import com.google.mlkit.vision.common.InputImage
 import com.google.mlkit.vision.face.Face
 import com.google.mlkit.vision.face.FaceDetection
+import java.io.ByteArrayOutputStream
+import java.io.File
+import java.io.FileOutputStream
+import java.io.OutputStream
 import java.util.concurrent.ExecutorService
+
 
 @ExperimentalGetImage class FaceRegistrationFragment : Fragment(), ImageAnalysis.Analyzer {
 
@@ -46,7 +62,7 @@ import java.util.concurrent.ExecutorService
         binding.lifecycleOwner = viewLifecycleOwner
 
         binding.apply {
-
+            faceRegistrationFragment = this@FaceRegistrationFragment
         }
     }
 
@@ -94,6 +110,47 @@ import java.util.concurrent.ExecutorService
         }, ContextCompat.getMainExecutor(requireContext()))
     }
 
+    fun takePicture() {
+        imageCapture?.takePicture(
+            ContextCompat.getMainExecutor(requireContext()),
+            object : ImageCapture.OnImageCapturedCallback() {
+                override fun onCaptureSuccess(imageProxy: ImageProxy) {
+                    // TODO: passing image for processing here e.g. pass into model to extract face vector
+                    Log.d(TAG, "Start processing here")
+
+                    val mediaImage = imageProxy.image
+
+                    if (mediaImage != null) {
+                        // Prepare input image using CameraX
+                        val image = InputImage.fromMediaImage(mediaImage, imageProxy.imageInfo.rotationDegrees)
+
+                        // Get base face detection model
+                        val detector = FaceDetection.getClient()
+
+                        // Event listener for image processing
+                        detector.process(image)
+                            .addOnSuccessListener { faces: List<Face> ->
+                                // Drawing face bounding box on overlay
+                                val faceBounds: MutableList<Rect> = arrayListOf()
+                                for (face in faces) {
+                                    // TODO: Crop only face
+
+                                    // TODO: Passing to model, retrieve feature vector
+                                }
+
+                                // Close before starting new image analysis
+                                imageProxy.close()
+                            }
+                            .addOnFailureListener {
+                                Log.e(TAG, it.toString(), it)
+                                imageProxy.close()
+                            }
+                    }
+                }
+            }
+        )
+    }
+
     override fun onDestroy() {
         super.onDestroy()
         cameraExecutor.shutdown()
@@ -131,6 +188,78 @@ import java.util.concurrent.ExecutorService
                     Log.e(TAG, it.toString(), it)
                     imageProxy.close()
                 }
+        }
+    }
+
+    fun ImageProxy.convertImageProxyToBitmap(): Bitmap {
+        val buffer = planes[0].buffer
+        buffer.rewind()
+        val bytes = ByteArray(buffer.capacity())
+        buffer.get(bytes)
+        return BitmapFactory.decodeByteArray(bytes, 0, bytes.size)
+    }
+
+    private fun croppedBitmap(mediaImage: Image, cropRect: Rect): Bitmap {
+        val yBuffer = mediaImage.planes[0].buffer // Y
+        val vuBuffer = mediaImage.planes[2].buffer // VU
+
+        val ySize = yBuffer.remaining()
+        val vuSize = vuBuffer.remaining()
+
+        val nv21 = ByteArray(ySize + vuSize)
+
+        yBuffer.get(nv21, 0, ySize)
+        vuBuffer.get(nv21, ySize, vuSize)
+
+        val yuvImage = YuvImage(nv21, ImageFormat.NV21, mediaImage.width, mediaImage.height, null)
+        val outputStream = ByteArrayOutputStream()
+        yuvImage.compressToJpeg(cropRect, 100, outputStream)
+        val imageBytes = outputStream.toByteArray()
+
+        return BitmapFactory.decodeByteArray(imageBytes, 0, imageBytes.size)
+    }
+
+    fun saveMediaToStorage(bitmap: Bitmap) {
+        //Generating a file name
+        val filename = "${System.currentTimeMillis()}.jpg"
+
+        //Output stream
+        var fos: OutputStream? = null
+
+        //For devices running android >= Q
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+            //getting the contentResolver
+            context?.contentResolver?.also { resolver ->
+
+                //Content resolver will process the contentvalues
+                val contentValues = ContentValues().apply {
+
+                    //putting file information in content values
+                    put(MediaStore.MediaColumns.DISPLAY_NAME, filename)
+                    put(MediaStore.MediaColumns.MIME_TYPE, "image/jpg")
+                    put(MediaStore.MediaColumns.RELATIVE_PATH, Environment.DIRECTORY_PICTURES)
+                }
+
+                //Inserting the contentValues to contentResolver and getting the Uri
+                val imageUri: Uri? =
+                    resolver.insert(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, contentValues)
+
+                //Opening an outputstream with the Uri that we got
+                fos = imageUri?.let { resolver.openOutputStream(it) }
+            }
+        } else {
+            //These for devices running on android < Q
+            //So I don't think an explanation is needed here
+            val imagesDir =
+                Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES)
+            val image = File(imagesDir, filename)
+            fos = FileOutputStream(image)
+        }
+
+        fos?.use {
+            //Finally writing the bitmap to the output stream that we opened
+            bitmap.compress(Bitmap.CompressFormat.JPEG, 100, it)
+            Toast.makeText(requireContext(), "Saved to Photos", Toast.LENGTH_SHORT).show()
         }
     }
 
