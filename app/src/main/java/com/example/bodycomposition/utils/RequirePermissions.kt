ae9651b9ae02a60f377eaeb1e96bcc57065ccf14
Natchanon Manatphaiboon <natchanon.space@gmail.com>
2023-10-14 11:36:36 +0700

    fix scale dynamic permissions

--- app/src/main/java/com/example/bodycomposition/utils/RequirePermissions.kt
+++ app/src/main/java/com/example/bodycomposition/utils/RequirePermissions.kt
@@ -3,6 +3,7 @@ package com.example.bodycomposition.utils
 import android.Manifest
 import android.content.pm.PackageManager
 import android.os.Build
+import android.util.Log
 import android.widget.Toast
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.core.content.ContextCompat
@@ -15,20 +16,23 @@ object RequirePermissions {
             Manifest.permission.CAMERA,
         ).apply {
             // CameraX permissions
-            if (Build.VERSION.SDK_INT <= Build.VERSION_CODES.P) {
+            if (Build.VERSION.SDK_INT < 30) {
                 add(Manifest.permission.WRITE_EXTERNAL_STORAGE)
             }
 
             // Scale permissions
-            if (Build.VERSION.SDK_INT >= 23) {
+            if (Build.VERSION.SDK_INT in 23..30) {
                 // android version 6-11
                 add(Manifest.permission.ACCESS_COARSE_LOCATION)
                 add(Manifest.permission.ACCESS_FINE_LOCATION)
-                // android version >= 12
-                // add(Manifest.permission.BLUETOOTH_CONNECT)
+            }
+            if (Build.VERSION.SDK_INT >= 31) {
+                add(Manifest.permission.BLUETOOTH_CONNECT)
+                add(Manifest.permission.BLUETOOTH_SCAN)
             }
         }.toTypedArray()
 
+    // TODO: Update dynamic permissions request
     fun requestPermissions(fragment: Fragment) {
         val activityResultLauncher =
             fragment.registerForActivityResult(
@@ -52,8 +56,25 @@ object RequirePermissions {
     }
 
     // Return true if all permission granted
-    fun allPermissionsGranted(fragment: Fragment) = REQUIRED_PERMISSIONS.all {
-        ContextCompat.checkSelfPermission(
-            fragment.requireContext(), it) == PackageManager.PERMISSION_GRANTED
+//    fun allPermissionsGranted(fragment: Fragment) = REQUIRED_PERMISSIONS.all {
+//        ContextCompat.checkSelfPermission(
+//            fragment.requireContext(), it) == PackageManager.PERMISSION_GRANTED
+//    }
+
+    fun allPermissionsGranted(fragment: Fragment): Boolean {
+        var check = true
+
+        for (permission in REQUIRED_PERMISSIONS) {
+            if (ContextCompat.checkSelfPermission(fragment.requireContext(), permission) == PackageManager.PERMISSION_GRANTED) {
+                Log.d(TAG, "$permission granted")
+            } else {
+                check = false
+                Log.d(TAG, "$permission denied")
+            }
+        }
+
+        return check
     }
+
+    private const val TAG = "RequirePermissions"
 }
